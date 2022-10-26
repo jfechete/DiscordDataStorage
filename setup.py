7afb0096b22de060f97755cd0fff8dabdf38f218
jfechete <60266095+jfechete@users.noreply.github.com>
2022-10-26 12:18:32 -0700

    Creating inital files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "discord_data_storage",
+    description = "Encrypted data storage with a format based on Discord",
+    url = "https://github.com/jfechete/DiscordDataStorage",
+    author = "jfechete",
+    packages = ["discord_data_storage"]
+)
