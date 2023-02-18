1efcb9b87f4d5892beed37f60d9434086c9b8c59
jfechete <60266095+jfechete@users.noreply.github.com>
2023-02-18 14:22:58 -0800

    Made read and write methods

--- discord_data_storage/discord_data_storage.py
+++ discord_data_storage/discord_data_storage.py
@@ -1,4 +1,6 @@
 import os
+import copy
+import json
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 
@@ -19,6 +21,22 @@ class DataAccessor:
         if not os.path.isdir(self._storage_location):
             os.makedirs(self._storage_location)
 
+    def read(self, server_id = "", user_id = ""):
+        template = self._get_template(server_id != "", user_id != "")
+        if template == None and not self._file_exists(server_id, user_id):
+            raise FileNotFoundError("No data file found")
+
+        if not self._file_exists(server_id, user_id):
+            data = copy.deepcopy(template)
+        else:
+            data = json.loads(self._read_file(server_id, user_id))
+            if isinstance(data, dict) and isinstance(template, dict):
+                DataAccessor._apply_template(data, template)
+        return data
+
+    def write(self, data, server_id = "", user_id = ""):
+        self._write_file(json.dumps(data), server_id, user_id)
+
     def _read_file(self, server_id = "", user_id = ""):
         with open(self._get_file_path(server_id, user_id), "r") as data_file:
             data = data_file.read()
@@ -31,6 +49,16 @@ class DataAccessor:
 
     def _file_exists(self,  server_id = "", user_id = ""):
         return os.path.isfile(self._get_file_path(server_id, user_id))
+
+    def _get_template(self, is_server, is_user):
+        if not is_server and not is_user:
+            return self._bot_template
+        elif not is_server:
+            return self._user_template
+        elif not is_user:
+            return self._server_template
+        else:
+            return self._member_template
 
     def _get_file_path(self, server_id = "", user_id = ""):
         return os.path.join(
@@ -52,3 +80,10 @@ class DataAccessor:
         kdf = Scrypt(salt=self._key, length=32, n=2**14, r=8, p=1)
         return kdf.derive(value.encode()).hex()
 
+    @staticmethod
+    def _apply_template(value, template):
+        for key in template:
+            if key not in value:
+                value[key] = copy.deepcopy(template[key])
+            elif isinstance(value[key], dict):
+                DataAccessor._apply_template(value[key], template[key])
