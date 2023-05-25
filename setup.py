c9797b09b4baffcf1c515a20267965cf53202aac
jfechete <60266095+jfechete@users.noreply.github.com>
2023-05-24 21:06:48 -0700

    Added long description

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md") as readme:
+    long_description = readme.read()
+
 setup(
     name = "discord_data_storage",
     description = "Encrypted data storage with a format based on Discord",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     url = "https://github.com/jfechete/DiscordDataStorage",
     author = "jfechete",
     packages = ["discord_data_storage"],
